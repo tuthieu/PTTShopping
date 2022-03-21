8afb57bc0d06e6cacb3c4562b234f7ea79231916
tuthieu <dangtuthieu@gmail.com>
2022-03-21 22:51:49 +0700

    Add basic webpage template

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "PTTShopping - Home" });
+});
+
+router.get("/index", function (req, res, next) {
+  res.render("index", { title: "PTTShopping - Home" });
+});
+
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "PTTShopping - About" });
+});
+
+router.get("/shop", function (req, res, next) {
+  res.render("shop", { title: "PTTShopping - Shop" });
+});
+
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "PTTShopping - Contact" });
 });
 
 module.exports = router;
