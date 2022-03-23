a2b61e3e3639b69edd4b90794f528b105be8dc3c
tuthieu <dangtuthieu@gmail.com>
2022-03-23 11:27:41 +0700

    Add static pages

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,36 @@ router.get("/contact", function (req, res, next) {
   res.render("contact", { title: "PTTShopping - Contact" });
 });
 
+router.get("/checkout", function (req, res, next) {
+  res.render("checkout", { title: "PTTShopping - Checkout" });
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("login", { title: "PTTShopping - Login" });
+});
+
+router.get("/register", function (req, res, next) {
+  res.render("register", { title: "PTTShopping - Register" });
+});
+
+router.get("/admin_manage", function (req, res, next) {
+  res.render("admin_manage", { title: "PTTShopping - Manage user" });
+});
+
+router.get("/admin_login", function (req, res, next) {
+  res.render("admin_login", { title: "PTTShopping - Admin login" });
+});
+
+router.get("/admin_info", function (req, res, next) {
+  res.render("admin_info", { title: "PTTShopping - Admin Information" });
+});
+
+router.get("/info", function (req, res, next) {
+  res.render("info", { title: "PTTShopping - Personal Information" });
+});
+
+router.get("/cart", function (req, res, next) {
+  res.render("cart", { title: "PTTShopping - Shopping Cart" });
+});
+
 module.exports = router;
